4a1fa97024100ec317bb932d55db07f418e19ca1
chebzy <34426050+chebzy@users.noreply.github.com>
2017-12-11 13:25:54 -0500

    stiffness of an ODE

--- stiffnessexplanation.m
+++ stiffnessexplanation.m
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 delta = 0.000001;
 F = inline('y^2 - y^3','t','y');
 opts = odeset('RelTol',1.e-4);
@@ -7,4 +8,9 @@ delta = 0.000001;
 F = inline('y^2 - y^3','t','y');
 opts = odeset('RelTol',1.e-4);
 >>>>>>> origin/master
+=======
+delta = 0.000001;
+F = inline('y^2 - y^3','t','y');
+opts = odeset('RelTol',1.e-4);
+>>>>>>> 1465271a7a1f595c106840d24fa18e5b1f37f70c
 ode45(F,[0 2/delta],delta,opts);
