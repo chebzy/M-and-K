f052530935f7b9547220d0c33e3268bdade8370b
Charles Basenga Kiyanda <personal@charleskiyanda.com>
2017-12-15 15:50:37 -0500

    Suggesting plot syntax
    --current state does not work, need to update the syntax

--- stiffnessexplanation.m
+++ stiffnessexplanation.m
@@ -1,4 +1,6 @@
 delta = 0.000001;
 F = inline('y^2 - y^3','t','y');
 opts = odeset('RelTol',1.e-4);
-ode45(F,[0 2/delta],delta,opts);
+solution = ode45(F,[0 2/delta],delta,opts);
+
+plot(solution[0],solution[1])
