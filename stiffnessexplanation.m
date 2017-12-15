46ea0c8d2fdcec60f85a527dcb8d1cd51f4e6d6f
Charles Basenga Kiyanda <personal@charleskiyanda.com>
2017-12-15 15:36:48 -0500

    Resetting example file to a proper state
    --removing extraneous git markup refering to merge conflicts

--- stiffnessexplanation.m
+++ stiffnessexplanation.m
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 delta = 0.000001;
 F = inline('y^2 - y^3','t','y');
 opts = odeset('RelTol',1.e-4);
-=======
-delta = 0.000001;
-F = inline('y^2 - y^3','t','y');
-opts = odeset('RelTol',1.e-4);
->>>>>>> origin/master
-=======
-delta = 0.000001;
-F = inline('y^2 - y^3','t','y');
-opts = odeset('RelTol',1.e-4);
->>>>>>> 1465271a7a1f595c106840d24fa18e5b1f37f70c
-=======
-delta = 0.000001;
-F = inline('y^2 - y^3','t','y');
-opts = odeset('RelTol',1.e-4);
->>>>>>> 1465271a7a1f595c106840d24fa18e5b1f37f70c
 ode45(F,[0 2/delta],delta,opts);
